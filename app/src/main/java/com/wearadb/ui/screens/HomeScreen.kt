1606b83e6a00bd14fe66db96033b31f4b9ffa52f
xinxiangjueming <kongjunhuai@gmail.com>
2026-09-14 17:14:20 +0800

    更改心形点击后的颜色

--- app/src/main/java/com/wearadb/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/wearadb/ui/screens/HomeScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.*
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
